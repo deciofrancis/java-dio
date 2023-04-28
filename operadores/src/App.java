ba936bbb2dd4567912bdd9419b0237839f236056
Décio Francis <francisdasilva@gmail.com>
2023-04-27 21:41:10 -0300

    operadores, metodos e exercicios

---
+++ operadores/src/App.java
@@ -0,0 +1,68 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+                
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+
+        // classe Operadores.java
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+        */
+
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a==b) ? "verdadeiro" : "false";
+
+       
+
+        String nome1 = "JAVA";
+        String nome2 = "JAVA";
+        
+        System.out.println(nome1 == nome2); //true
+
+        String nome3 = new String("JAVA");
+        
+        System.out.println(nome1 == nome3); //false
+
+        String nome4 = nome3;
+
+        System.out.println(nome3 == nome4); //true
+        
+        //equals na parada
+        System.out.println(nome1.equals(nome2)); //??
+        System.out.println(nome2.equals(nome3)); //??
+        System.out.println(nome3.equals(nome4)); //??
+
+        System.out.println("https://glysns.gitbook.io/java-basico/sintaxe/operadores");
+    }
+}
