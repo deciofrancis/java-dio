c312753c20ed0077faa5b0ddbdf74c200b02f2b7
Décio Francis <francisdasilva@gmail.com>
2023-05-22 22:56:44 -0300

    continuacao do exercicio de populacoes

--- collections-list/src/Ex5_Populacao.java
+++ collections-list/src/Ex5_Populacao.java
@@ -11,6 +11,24 @@ Dada a população estimada de alguns estados do nordeste brasileiro, faça:
  Confira se o estado PB está no dicionário, caso não adicione: PB -
 4.039.277;
 Exiba a população PE;
+Exiba todos os estados e suas populações na ordem que foi
+informado;
+
+Exiba os estados e suas populações em ordem alfabética;
+
+Exiba o estado com o menor população e sua quantidade;
+
+Exiba o estado com a maior população e sua quantidade;
+
+Exiba a soma da população desses estados;
+
+Exiba a média da população deste dicionário de estados;
+
+Remova os estados com a população menor que 4.000.000;
+
+Apague o dicionário de estados;
+
+Confira se o dicionário está vazio.
  */
 
 public class Ex5_Populacao {
@@ -34,5 +52,52 @@ public class Ex5_Populacao {
         System.out.println(populaçãoEstadosNE);
 
         System.out.println("Exiba a população do estado PE: " + populaçãoEstadosNE.get("PE"));
+
+        System.out.println("Exiba todos os estados e suas populaçãos na ordem em que foram informados: ");
+        Map<String, Integer> populaçãoEstadosNE2 = new LinkedHashMap<>() {{
+            put("PE", 9616621);
+            put("AL", 3351543);
+            put("CE", 9187103);
+            put("RN", 3534265);
+            put("PB", 4039277);
+        }};
+        System.out.println(populaçãoEstadosNE2);
+
+        System.out.println("Exiba todos os estados e suas populações na ordem alfabética: ");
+        Map<String, Integer> populaçãoEstadosNE3 = new TreeMap<>(populaçãoEstadosNE);
+        System.out.println(populaçãoEstadosNE3);
+
+        Collection<Integer> populacao = populaçãoEstadosNE.values();
+        String estadoMaiorPopulacao = "";
+        String estadoMenorPopulacao = "";
+        for (Map.Entry<String, Integer> entry : populaçãoEstadosNE.entrySet()) {
+            if (entry.getValue().equals(Collections.max(populacao))) estadoMaiorPopulacao = entry.getKey();
+            if (entry.getValue().equals(Collections.min(populacao))) estadoMenorPopulacao = entry.getKey();
+        }
+        System.out.printf("Exiba o estado com o menor população (%s) e seu respectivo valor (%d)\n",
+                estadoMenorPopulacao, Collections.min(populacao));
+        System.out.printf("Exiba o estado com a maior população (%s) e seu respectivo valor (%d)\n",
+                estadoMaiorPopulacao, Collections.max(populacao));
+
+        int soma = 0;
+        Iterator<Integer> iterator = populaçãoEstadosNE.values().iterator();
+        while (iterator.hasNext()) {
+            soma += iterator.next();
+        }
+        System.out.println("Exiba a soma da população desses estados: " + soma);
+
+        System.out.println("Exiba a média da população deste dicionário de estados: " + (soma / populaçãoEstadosNE.size()));
+        System.out.println("Remova os estados com a população menor que 4.000.000: ");
+        Iterator<Integer> iterator1 = populaçãoEstadosNE.values().iterator();
+        while (iterator1.hasNext()) {
+            if (iterator1.next() <= 4000000) iterator1.remove();
+        }
+        System.out.println(populaçãoEstadosNE);
+
+        System.out.println("Apague o dicionario de estados com suas respectivas populações estimadas: ");
+        populaçãoEstadosNE.clear();
+        System.out.println(populaçãoEstadosNE);
+
+        System.out.println("Confira se a lista está vazia: " + populaçãoEstadosNE.isEmpty());
     }
 }
